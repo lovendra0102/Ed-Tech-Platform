77e7878943e9d842ef6ac5f7a4d15bb64355ffb6
lovendra0102 <lovendradtu01020@gmail.com>
2025-04-09 01:21:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json()); // to parse json body
 app.use(cookieParser());
 app.use(
     cors({
-        origin: 'http://localhost:5173', // frontend link
+        origin: 'https://ed-tech-platform-frontend.onrender.com', // frontend link
         // origin: process.env.FRONTEND_URL,
         // origin: "*",
         credentials: true
