fc2593089b28377c8338ac24804ea447be31af3f
lovendra0102 <lovendradtu01020@gmail.com>
2025-04-09 13:04:53 +0530

    Update Footer.jsx

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -178,7 +178,7 @@ const Footer = () => {
 
               <Link to='https://github.com/Aniruddha-Gade' target="__blank" className="text-white hover:underline mr-1"
               >
-                Adarsh
+                Lovendra
               </Link>
             </div>
             <span> © 2025 Studynotion</span>
