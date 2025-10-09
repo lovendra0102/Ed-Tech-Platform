130238e092bb2c41004dd3976af07f50f82a7296
lovendra0102 <lovendradtu01020@gmail.com>
2025-10-10 00:57:07 +0530

    Update Footer.jsx

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -174,7 +174,7 @@ const Footer = () => {
 
           <div className="text-center flex flex-col sm:flex-row ">
             <div className="flex ">
-              <span> Made with ❤️</span>
+              <span> Made with ❤️Lovendra</span>
 
               <Link to='https://github.com/Aniruddha-Gade' target="__blank" className="text-white hover:underline mr-1"
               >
@@ -185,10 +185,10 @@ const Footer = () => {
           </div>
 
           <div className="flex items-center">
-            <a href="https://www.linkedin.com/in/rajat-saroj-5829b6315/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/lovendra-kumar-ba8638270/" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImLinkedin2 size={17} />
             </a>
-            <a href="https://github.com/Rajat-Saroj" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/lovendra0102" className="text-white p-3 hover:bg-richblack-700 rounded-full duration-300" target="_blank" rel="noopener noreferrer">
               <ImGithub size={17} />
             </a>
           </div>
